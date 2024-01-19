95fe29e1398301269c9831c3ee4936682b7da1a1
Alex Jolly <alexjolly28@gmail.com>
2024-01-19 12:15:47 +0530

    matching fucntion added

--- src/hello_world_backend/src/lib.rs
+++ src/hello_world_backend/src/lib.rs
@@ -1,4 +1,26 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
+use ic_cdk_macros::{query, export_candid};
+use std::cmp::Ordering;
+
+
+#[query]
+fn hello(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[query]
+fn matching(a: i32,b:i32) -> String {
+
+    match a.cmp(&b) {
+        Ordering::Less => format!("{} is big",b),
+        Ordering::Greater => format!("{} Too big!",a),
+        Ordering::Equal => format!("You win!"),
+
+    }
+
+}
+
+
+
+// Generate did files
+
+export_candid!();
