d32b8722ed2e376d0ac213bd7ef93b82379ce07f
Alex Jolly <alexjolly28@gmail.com>
2024-07-09 02:16:24 +0530

    SIMD optimisation added

--- ic-tokenizer/build.sh
+++ ic-tokenizer/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-# export RUSTFLAGS=$RUSTFLAGS' -C target-feature=+simd128'
+export RUSTFLAGS=$RUSTFLAGS' -C target-feature=+simd128'
 cargo build --release --target=wasm32-wasi
 wasi2ic ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm
-# wasm-opt -Os -o ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm \
-#         ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm
+wasm-opt -Os -o ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm \
+        ./target/wasm32-wasi/release/ic_tokenizer_backend.wasm
 
 # candid-extractor "target/wasm32-wasi/release/ic_tokenizer_backend.wasm" > "src/ic-tokenizer-backend/ic-tokenizer-backend.did"
