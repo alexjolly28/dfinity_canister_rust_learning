05a3b463ff6d91b1633f16e9c215510b383a5f06
Alex Jolly <alexjolly28@gmail.com>
2024-09-10 01:16:50 +0530

    storage updated

--- ic-tokenizer/src/ic-tokenizer-backend/src/lib.rs
+++ ic-tokenizer/src/ic-tokenizer-backend/src/lib.rs
@@ -1,3 +1,7 @@
+mod storage;
+
+use bytes::Bytes;
+
 use ic_stable_structures::{
     memory_manager::{MemoryId, MemoryManager},
     DefaultMemoryImpl,
@@ -7,16 +11,17 @@ use rust_tokenizers::vocab::BertVocab;
 use std::cell::RefCell;
 
 const WASI_MEMORY_ID: MemoryId = MemoryId::new(0);
+const VOCAB_PATH: &str = "bert-base-uncased-vocab.txt";
+const TARGET_LEN: usize = 256;
 
 thread_local! {
+
+    static VOCAB: RefCell<Option<BertVocab>>=RefCell::new(None);
+
     // The memory manager is used for simulating multiple memories.
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
-}
 
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
 }
 
 #[ic_cdk::init]
@@ -31,29 +36,85 @@ fn post_upgrade() {
     ic_wasi_polyfill::init_with_memory(&[0u8; 32], &[], wasi_memory);
 }
 
-const VOCAB: &'static [u8] = include_bytes!("../assets/bert-base-uncased-vocab.txt");
+#[ic_cdk::update]
+fn append_bytes(bytes: Vec<u8>) {
+    storage::append_bytes(VOCAB_PATH, bytes);
+}
+
+pub fn setup_vocab(vocab_bytes: Bytes) -> Result<(), ()> {
+    // let v = vocab_bytes.to_vec();
+    let vocab = BertVocab::from_bytes(&vocab_bytes.to_vec()).unwrap();
+    VOCAB.with_borrow_mut(|m| {
+        *m = Some(vocab);
+    });
+    Ok(())
+}
+
+#[ic_cdk::update]
+fn setup_models() -> Result<(), String> {
+    setup_vocab(storage::bytes(VOCAB_PATH))
+        .map_err(|err| format!("Failed to setup model: {:?}", err))
+}
+
+fn pad_vector<T: Default + Clone>(input_ids: Vec<T>, target_len: usize) -> Vec<T> {
+    let mut padded = input_ids.clone();
+    padded.resize(target_len, T::default());
+    padded
+}
+
+fn fmt(n: u64) -> String {
+    n.to_string()
+        .as_bytes()
+        .rchunks(3)
+        .rev()
+        .map(std::str::from_utf8)
+        .collect::<Result<Vec<&str>, _>>()
+        .unwrap()
+        .join("_")
+}
 
 #[ic_cdk::query]
-fn tokenize(text: String) -> (Vec<i64>, Vec<i64>) {
-    let vocab = BertVocab::from_bytes(VOCAB).unwrap();
+fn tokenize(sentence: String) -> Result<(Vec<i64>, Vec<i32>, Vec<i8>), ()> {
+    let instructions_before = ic_cdk::api::instruction_counter();
 
     let lowercase: bool = true;
     let strip_accents: bool = true;
+    VOCAB.with_borrow(|vocab| {
+        let vocab = vocab.clone().unwrap();
 
-    let bert_tokenizer = BertTokenizer::from_existing_vocab(vocab, lowercase, strip_accents);
+        let bert_tokenizer = BertTokenizer::from_existing_vocab(vocab, lowercase, strip_accents);
 
-    let tokens = bert_tokenizer.encode(&text, None, 128, &TruncationStrategy::LongestFirst, 0);
-    ic_cdk::println!("{:?}", tokens);
-    let input_ids = tokens.token_ids;
+        let tokens = bert_tokenizer.encode(
+            &sentence,
+            None,
+            TARGET_LEN,
+            &TruncationStrategy::DoNotTruncate,
+            0,
+        );
 
-    // Generate and print the attention mask
+        let input_ids = tokens.token_ids;
+        let input_ids: Vec<i64> = pad_vector(input_ids, TARGET_LEN);
 
-    let attention_mask: Vec<i64> = input_ids
-        .iter()
-        .map(|&id| if id == 0 { 0 } else { 1 })
-        .collect();
+        // Generate and print the attention mask
+        let attention_mask = input_ids
+            .iter()
+            .map(|&id| if id == 0 { 0 } else { 1 })
+            .collect();
 
-    (input_ids, attention_mask)
+        let attention_mask = pad_vector(attention_mask, TARGET_LEN);
+
+        let segment_ids = tokens.segment_ids;
+        let token_type_ids = pad_vector(segment_ids, TARGET_LEN);
+
+        let instructions = ic_cdk::api::instruction_counter() - instructions_before;
+
+        ic_cdk::println!(
+            "Tokenization:     {:>12} Wasm instructions",
+            fmt(instructions)
+        );
+
+        Ok((input_ids, attention_mask, token_type_ids))
+    })
 }
 
-// ic_cdk::export_candid!();
+ic_cdk::export_candid!();
